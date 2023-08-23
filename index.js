8172ffe9640e9397953b7ce86f6d5637f9d001f7
Stephen Rivas Jr <sprjr@me.com>
2023-08-23 13:23:59 -0400

    Update index.js
    Robot teaches algo performance variations.

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ function binarySearch(sortedInputArray, targetValue) {
     // will allow you to miss; `>=` will allow you to run once more to find the final
     // targetValue (if available in the arry at all)
     while (max >= min) {
+        // interestingly, this solution worked for the LeetCode problem but the performance rating (beats 19%) was a LOT lower
+        // than I thought. I discussed this vs the fastest solution with ChatGPT - and it appears that while functionally the
+        // same, using Math.ceil() can result in quicker timings. From the robot:
+        // This is a subtle difference. You're using Math.floor() and the provided solution uses Math.ceil(). 
+        // Both approaches can work, but there might be some slight differences in terms of the number of iterations especially 
+        // when the array length is odd. For most practical purposes, either approach is valid.
+
+        // TIL. Moving this to .ceil() instead of .floor() in that test results in a beats 73% perf rating.
         guess = Math.floor((max+min) / 2);
         if (sortedInputArray[guess] === targetValue) {
             return guess;
